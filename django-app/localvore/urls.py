f7a6e65a4f10c4b05f054715781cb76ccaea67a9
Paul Bailey <paul.m.bailey@gmail.com>
2014-12-06 15:02:07 +0000

    setup dev urls

--- django-app/localvore/urls.py
+++ django-app/localvore/urls.py
@@ -2,9 +2,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'localvore.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+  # Examples:
+  # url(r'^$', 'localvore.views.home', name='home'),
+  # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+  url(r'^admin/', include(admin.site.urls)),
 )
+
+import os
+from django.conf import settings
+
+if settings.DEBUG:
+  from django.views.static import serve
+  
+  ng_path = os.path.join(os.path.dirname(settings.BASE_DIR), 'ng-app')
+  urlpatterns += [url(r'^(?P<path>.*)$', serve, kwargs={'document_root': ng_path})]
+  
