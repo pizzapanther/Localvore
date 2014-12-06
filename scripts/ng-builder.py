144c5de1acba03629b9dad9f0d8de14b5fa7f4dd
Paul Bailey <paul.m.bailey@gmail.com>
2014-12-06 16:15:22 +0000

    setup build script

--- scripts/ng-builder.py
+++ scripts/ng-builder.py
@@ -1,42 +1,75 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import os
+import time
 import datetime
 import subprocess
 
 import pyinotify
 
-class LessCompile (pyinotify.ProcessEvent):
-  def __init__ (self, ldir, *args, **kwargs):
-    self.ldir = ldir
-    super(LessCompile, self).__init__(*args, **kwargs)
+class Compile (pyinotify.ProcessEvent):
+  def __init__ (self, ngdir, *args, **kwargs):
+    self.ngdir = ngdir
+    self.ldir = os.path.join(self.ngdir, 'css')
+    
+    super(Compile, self).__init__(*args, **kwargs)
     
   def process_IN_CLOSE (self, event):
-    if event.mask == pyinotify.IN_CLOSE_WRITE and event.pathname.endswith('.less'):
-      self.do_compile()
+    build = False
+    if event.mask == pyinotify.IN_CLOSE_WRITE:
+      if event.pathname.endswith('.less'):
+        self.less_compile()
+        build = True
+        
+      elif event.pathname.endswith('.js'):
+        build = True
+        
+      elif event.pathname.endswith('index.tpl.html'):
+        build = True
+        
+    if build:
+      self.build_html()
       
-  def do_compile (self):
-    print("Processing main.less " + unicode(datetime.datetime.now()))
+  def less_compile (self):
+    print("Processing localvore.less " + unicode(datetime.datetime.now()))
     subprocess.call(
       "lessc {ldir}/localvore.less {ldir}/localvore.css".format(ldir=self.ldir),
       shell=True
     )
-    build_html()
     
-def build_html ():
-  print "narf"
-  
+  def compile_all (self):
+    self.less_compile()
+    self.build_html()
+    
+  def build_html (self):
+    tpl = os.path.join(self.ngdir, 'index.tpl.html')
+    html = os.path.join(self.ngdir, 'index.html')
+    
+    ts = '?ts={}'.format(int(time.time()))
+    
+    fh = open(tpl, 'r')
+    content = fh.read()
+    fh.close()
+    
+    content = content.replace('[[ ts ]]', ts)
+    
+    fh = open(html, 'w')
+    fh.write(content)
+    fh.close()
+    
 def run_builder ():
-  basedir = os.path.abspath(os.path.dirname(__file__))
-  os.chdir(basedir)
+  BASEDIR = os.path.abspath(os.path.dirname(__file__))
+  os.chdir(BASEDIR)
   
-  ldir = os.path.join(basedir, '..', 'ng-app', 'css')
+  ngdir = os.path.join(BASEDIR, '..', 'ng-app')
   
   wm = pyinotify.WatchManager()
-  handler = LessCompile(ldir)
-  handler.do_compile()
+  handler = Compile(ngdir)
+  handler.compile_all()
   notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
-  wm.add_watch(ldir, pyinotify.ALL_EVENTS, rec=True, auto_add=True)
+  wm.add_watch(ngdir, pyinotify.ALL_EVENTS, rec=True, auto_add=True)
   notifier.loop()
   
 if __name__ == "__main__":
