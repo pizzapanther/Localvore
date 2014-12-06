2d8110e4453315a79a6a37eec83e8f9d07cb6c94
Paul Bailey <paul.m.bailey@gmail.com>
2014-12-06 22:09:47 +0000

    update urls

--- django-app/localvore/settings/__init__.py
+++ django-app/localvore/settings/__init__.py
@@ -102,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/www/Localvore/static/'
 
 from localvore.settings.private.main import *
 
