5792e409ac2a815b1b4aa2fcef1810f2293ba5d5
chriscauley <chris@lablackey.com>
2014-12-07 11:58:59 -0500

    resolves #12

--- django-app/place/views.py
+++ django-app/place/views.py
@@ -22,8 +22,16 @@ def places_json (request):
   #places = places.filter(geopoint__distance_lte=(geopoint,D(mi=miles))).distance(geopoint).order_by('distance')
   places = places.distance(geopoint).order_by('distance')[:200]
   geolocator = GoogleV3()
-  return HttpResponse(dumps([p.as_json for p in places]))
+  _jsons = [p.as_json for p in places]
+  for json,place in zip(_jsons,places):
+    json['distance'] = place.distance.m*0.00062137119223733
+  return HttpResponse(dumps(_jsons))
 
 def place_detail_json (request,pk):
   place = get_object_or_404(Place,pk=pk)
-  return HttpResponse(dumps(place.full_json))
+  json = place.full_json
+  lat = request.GET.get('lat',29.7604267) #y
+  lon = request.GET.get('lon',-95.3698028) #x
+  geopoint = fromstr('POINT(%s %s)'%(lon,lat))
+  json['distance'] = place.geopoint.distance(geopoint)*0.00062137119223733
+  return HttpResponse(dumps(json))
