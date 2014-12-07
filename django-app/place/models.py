cab45fc7f6a21db9aa5645b5b879d3a29690a09e
Paul Bailey <paul.m.bailey@gmail.com>
2014-12-07 17:43:08 +0000

    fixed api

--- django-app/place/models.py
+++ django-app/place/models.py
@@ -62,7 +62,10 @@ class Place(GeoModel):
     _d['description'] = striptags(_d['description'])
     _d['geopoint'] = self.geopoint.tuple
     _d['city'] = str(self.city)
-    _d['placetypes'] = [pt.as_json for pt in self.placetypes.all()]
+    _d['placetypes'] = []
+    if self.placetype:
+      _d['placetypes'] = [self.placetype.as_json]
+      
     _d['images'] = [i.as_json for i in self.images.all()]
     if self.image:
       _d['image'] = self.image.as_json
